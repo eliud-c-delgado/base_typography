66232d1cd6c43db30b6886f79be3d4de128cc57c
eliud-c-delgado <30604824+eliud-c-delgado@users.noreply.github.com>
2018-03-28 03:59:51 -0600

    Adding sidebar

--- scripts/main.babel.js
+++ scripts/main.babel.js
@@ -1,1 +1,11 @@
+const menuToggle = document.querySelector("#nav-toggle");
+const nav = document.querySelector("#index");
 
+
+
+menuToggle.addEventListener('click',
+  e => {
+    nav.classList.toggle('on');
+    e.stopPropagation();
+  }
+);
